e0dd6262bced662dd58e64ffb0340041a2e1452b
misung.dev <misung.dev@gmail.com>
2024-11-24 01:28:52 +0900

    feat: 미션방 상단에 메시지 box 구현

--- react/src/pages/today/index.tsx
+++ react/src/pages/today/index.tsx
@@ -1,5 +1,32 @@
-const Page = () => {
-  return <>today</>;
+import {
+  Container,
+  LetterIcon,
+  MessageBox,
+  MessageBoxText,
+  SenderToReceiver,
+  SenderToReceiverWrapper,
+} from './style';
+import Layout from '../../components/Layout/Layout';
+
+interface PageProps {
+  sender: string;
+  receiver: string;
+}
+
+const Page: React.FC<PageProps> = ({ sender, receiver }) => {
+  return (
+    <Layout headerTitle="미션방" showBackButton={true}>
+      <Container>
+        <MessageBox>
+          <SenderToReceiver>
+            <LetterIcon>💌</LetterIcon>
+            <SenderToReceiverWrapper>{`${sender}(나) -> ${receiver}`}</SenderToReceiverWrapper>
+          </SenderToReceiver>
+          <MessageBoxText>마음을 녹이는 커피를 사세요.</MessageBoxText>
+        </MessageBox>
+      </Container>
+    </Layout>
+  );
 };
 
 export default Page;
---
+++ react/src/pages/today/style.tsx
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  height: 100dvh;
+`;
+
+export const MessageBox = styled.div`
+  display: flex;
+  margin: 32px 20px;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 4px;
+  align-self: stretch;
+  padding: 16px 20px 20px 20px;
+  border-radius: 16px;
+  background: var(--Background-Normal-Alternative, #f7f7f8);
+`;
+
+export const SenderToReceiver = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const LetterIcon = styled.div`
+  color: var(--Semantic-Label-Normal, var(--Label-Normal, #171719));
+  text-align: center;
+  font-size: 24px;
+  line-height: 32px;
+  letter-spacing: 0.137px;
+`;
+
+export const SenderToReceiverWrapper = styled.p``;
+
+export const MessageBoxText = styled.p`
+  color: var(--Semantic-Label-Normal, var(--Label-Normal, #171719));
+  font-size: 16px;
+  line-height: 162.5%;
+  letter-spacing: 0.091px;
+`;
