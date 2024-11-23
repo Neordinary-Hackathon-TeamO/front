5d413d1649d291ec1dc7d5396ee9695361be8888
DongjaJ <jdh981118@naver.com>
2024-11-24 01:58:39 +0900

    feat: layout component에 titleColor, backgroundImageUrl 추가

--- react/src/pages/match/index.tsx
+++ react/src/pages/match/index.tsx
@@ -10,6 +10,8 @@ import {
 } from './style';
 
 import tempImageUrl from '../../assets/match-temp.svg';
+import Layout from '../../components/Layout/Layout';
+import backgroundURL from '../../assets/match-background.png';
 
 const Page = () => {
   const params = useParams();
@@ -17,7 +19,12 @@ const Page = () => {
   console.log(params);
 
   return (
-    <Container>
+    <Layout
+      backgroundImageURL={backgroundURL}
+      titleColor="#FFFFFF"
+      headerTitle="매칭 결과"
+      showBackButton
+    >
       <ManitoContainer>
         <ManitoMe>닉네임님의 마니또 </ManitoMe>
 
@@ -28,7 +35,7 @@ const Page = () => {
       <ButtonContainer>
         <MissionCheckButton>오늘 미션 확인하기</MissionCheckButton>
       </ButtonContainer>
-    </Container>
+    </Layout>
   );
 };
 
