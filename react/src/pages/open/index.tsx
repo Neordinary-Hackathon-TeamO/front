94c7379351a89ef19165803745ebc45f794c5dd6
Youjiiin <yujinjang24@gmail.com>
2024-11-24 06:58:14 +0900

    전체적 페이지 디자인, 방만들기 통신

--- react/src/pages/open/index.tsx
+++ react/src/pages/open/index.tsx
@@ -1,166 +1,97 @@
-import styled from 'styled-components';
-import background from '../../assets/match-background.png';
+import {
+  Wrapper,
+  Container,
+  Title,
+  Santa,
+  ResultContainer,
+  ResultTextContainer,
+  ResultNumberContainer,
+  ResultTitle,
+  ResultText,
+  Result,
+  ResultListContainer,
+  ResultList,
+  ResultName,
+  Arrow,
+} from './style';
 import santa from '../../assets/santa.svg';
-import arrow from "../../assets/ArrowRight.svg";
+import arrow from '../../assets/ArrowRight.svg';
+import Layout from '../../components/Layout/Layout';
+import background from '../../assets/match-background.png';
 
 const Page = () => {
+  const pairs = [
+    { from: '윰', to: '로지' },
+    { from: '로지', to: '윈터' },
+    { from: '윈터', to: '밤톨' },
+    { from: '밤톨', to: '동자' },
+    { from: '동자', to: '차차' },
+    { from: '차차', to: '올라잇' },
+    { from: '올라잇', to: '로빈' },
+    { from: '로빈', to: '윰' },
+  ];
+
+  const resultData = {
+    week: 15,
+    mission: 4,
+    checked: 18,
+    max: "윰"
+  };
+
   return (
-    <Wrapper>
-      <Container>
-        <Title>우리 팀, 이만큼 친해졌어요!</Title>
-        <ResultContainer>
-          <Santa src={santa} />
-          <ResultTitle>산타의 비밀친구들</ResultTitle>
-          <ResultText> 은</ResultText>
+    <Layout
+      headerTitle="정체 공개"
+      showBackButton
+      titleColor={'#FFF'}
+      backgroundImageURL={background}
+    >
+      <Wrapper>
+        <Container>
+          <Title>우리 팀, 이만큼 친해졌어요!</Title>
+          <ResultContainer>
+            <Santa src={santa} />
+            <ResultTitle>산타의 비밀친구들</ResultTitle>
+            <ResultText> 은</ResultText>
 
-          <ResultNumberContainer>
-            <ResultTextContainer>
-              <ResultTitle>👥 7일간 인증한 사람</ResultTitle>
-              <Result>15명</Result>
-            </ResultTextContainer>
-            <ResultTextContainer>
-              <ResultTitle>✅ 미션 인증 횟수</ResultTitle>
-              <Result>평균 4번</Result>
-            </ResultTextContainer>
-            <ResultTextContainer>
-              <ResultTitle>🎯 마니또 맞춘 사람 수</ResultTitle>
-              <Result>18명</Result>
-            </ResultTextContainer>
-            <ResultTextContainer>
-              <ResultTitle>👑 가장 많이 인증한 사람</ResultTitle>
-              <Result>김마니또</Result>
-            </ResultTextContainer>
-          </ResultNumberContainer>
+            <ResultNumberContainer>
+              <ResultTextContainer>
+                <ResultTitle>👥 7일간 인증한 사람</ResultTitle>
+                <Result>{resultData.week}명</Result>
+              </ResultTextContainer>
+              <ResultTextContainer>
+                <ResultTitle>✅ 미션 인증 횟수</ResultTitle>
+                <Result>평균 {resultData.mission}번</Result>
+              </ResultTextContainer>
+              <ResultTextContainer>
+                <ResultTitle>🎯 마니또 맞춘 사람 수</ResultTitle>
+                <Result>{resultData.checked}명</Result>
+              </ResultTextContainer>
+              <ResultTextContainer>
+                <ResultTitle>👑 가장 많이 인증한 사람</ResultTitle>
+                <Result>{resultData.max}</Result>
+              </ResultTextContainer>
+            </ResultNumberContainer>
 
-          <ResultText>
-            산타의 비밀친구들은 7일 동안 15명이 인증했어요. 7일 동안 평균 4번
-            인증했어요. 가장 많이 인증한 사람은 10번 인증한 김마니또님 이에요.
-            40명 중에 18명이 마니또가 누구인지 맞췄어요.
-          </ResultText>
-        </ResultContainer>
+            <ResultText>
+              산타의 비밀친구들은 7일 동안 {resultData.week}명이 인증했어요. 7일 동안 평균 {resultData.mission}번
+              인증했어요. 가장 많이 인증한 사람은 10번 인증한 {resultData.max}님 이에요.
+              40명 중에 {resultData.checked}명이 마니또가 누구인지 맞췄어요.
+            </ResultText>
+          </ResultContainer>
 
-        <ResultListContainer>
-          <ResultList>
-            <ResultName>닉네임</ResultName>
-            <Arrow src={arrow}/>
-            <ResultName>닉네임최대10글자임</ResultName>
-          </ResultList>
-          <ResultList>
-            <ResultName>닉네임</ResultName>
-            <Arrow src={arrow}/>
-            <ResultName>닉네임최대10글자임</ResultName>
-          </ResultList>
-          <ResultList>
-            <ResultName>닉네임</ResultName>
-            <Arrow src={arrow}/>
-            <ResultName>닉네임최대10글자임</ResultName>
-          </ResultList>
-          <ResultList>
-            <ResultName>닉네임</ResultName>
-            <Arrow src={arrow}/>
-            <ResultName>닉네임최대10글자임</ResultName>
-          </ResultList>
-        </ResultListContainer>
-      </Container>
-    </Wrapper>
+          <ResultListContainer>
+            {pairs.map((pair, index) => (
+              <ResultList key={index}>
+                <ResultName>{pair.from}</ResultName>
+                <Arrow src={arrow} />
+                <ResultName>{pair.to}</ResultName>
+              </ResultList>
+            ))}
+          </ResultListContainer>
+        </Container>
+      </Wrapper>
+    </Layout>
   );
 };
 
-const Wrapper = styled.div`
-  position: fixed;
-  background-image: url(${background});
-  width: 100%;
-  height: 100%;
-  background-size: cover;
-  overflow: hidden;
-`;
-const Container = styled.div`
-  overflow: scroll;
-  padding: 0 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 30px;
-  width: 100%;
-`;
-const Title = styled.h2`
-  font-size: 16px;
-  font-weight: 600;
-  line-height: line-height: 1.358;
-  color: #FFF;
-`;
-
-const Santa = styled.img`
-  width: 141px;
-  max-width: none;
-  height: 141px;
-  position: absolute;
-  top: 0;
-  left: 50%;
-  transform: translate(-50%, -81%) rotate(5.17deg);
-  transform-origin: left;
-  aspect-ratio: 1/1;
-`;
-const ResultContainer = styled.div`
-  position: relative;
-  margin-top: 130px;
-  background-color: #fff;
-  border-radius: 16px;
-  padding: 32px 16px;
-  width: 100%;
-`;
-const ResultTextContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-const ResultNumberContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-  margin: 16px 0 32px;
-`;
-const ResultTitle = styled.span`
-  font-size: 14px;
-  font-weight: 600;
-  line-height: 1.334;
-  color: #2e2f33;
-`;
-const ResultText = styled.span`
-  font-size: 14px;
-  font-weight: 400;
-  color: #2e2f33;
-  line-height: 1.571;
-`;
-const Result = styled.span`
-  color: #497C6B;
-  font-weight: 600;
-  font-size: 17px;
-  line-height: 1.358;
-`;
-
-const ResultListContainer = styled.div`
-  margin-top: 20px;
-  background-color: #fff;
-  border-radius: 16px;
-  padding: 20px 16px;
-  width: 100%;
-`;
-const ResultList = styled.div`
-  display: flex;
-  align-items: center;
-  margin: 2px 0;
-  height: 40px;
-`;
-const ResultName = styled.p`
-  font-size: 15px;
-  font-weight: 500;
-  color: #333D48;
-`;
-const Arrow = styled.img`
-  width: 24px;
-  height: 24px;
-  margin-left: auto;
-  margin-right: 8px;
-`;
 export default Page;
