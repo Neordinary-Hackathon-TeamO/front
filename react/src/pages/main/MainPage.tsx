bdd5f6bb99f8c4e1609855479a216fb672d594df
misung.dev <misung.dev@gmail.com>
2024-11-23 23:05:47 +0900

    feat: 초대 코드 미입력시 입장하기 버튼 비활성화 효과 적용

--- react/src/pages/main/MainPage.tsx
+++ react/src/pages/main/MainPage.tsx
@@ -1,39 +1,43 @@
+import { useState } from 'react';
 import RoomCard from '../../components/RoomCard/RoomCard';
 import * as S from './MainPage.style';
 
 const MainPage: React.FC = () => {
+  const [inputValue, setInputValue] = useState('');
   return (
-    <>
-      <S.MainPage>
-        <S.CreateRoomSection>
-          <S.CreateRoomButton>
-            <S.CreateRoomPlusIcon>
-              <img src="/public/icons/icon-plus.svg" alt="plus-icon" />
-            </S.CreateRoomPlusIcon>
-            <S.CreateRoomText>방 만들기</S.CreateRoomText>
-          </S.CreateRoomButton>
-        </S.CreateRoomSection>
+    <S.MainPage>
+      <S.CreateRoomSection>
+        <S.CreateRoomButton>
+          <S.CreateRoomPlusIcon>
+            <img src="/public/icons/icon-plus.svg" alt="plus-icon" />
+          </S.CreateRoomPlusIcon>
+          <S.CreateRoomText>방 만들기</S.CreateRoomText>
+        </S.CreateRoomButton>
+      </S.CreateRoomSection>
 
-        <S.InvitationCodeSection>
-          <S.InvitationCodeForm>
-            <S.InvitationCodeFormInputWrapper>
-              <S.InvitationCodeFormTitle>초대코드</S.InvitationCodeFormTitle>
-              <S.InvitationCodeFormInput placeholder="초대코드를 입력해주세요." />
-            </S.InvitationCodeFormInputWrapper>
-            <S.InvitationCodeSubmitButton>
-              입장하기
-            </S.InvitationCodeSubmitButton>
-          </S.InvitationCodeForm>
-        </S.InvitationCodeSection>
+      <S.InvitationCodeSection>
+        <S.InvitationCodeForm>
+          <S.InvitationCodeFormInputWrapper>
+            <S.InvitationCodeFormTitle>초대코드</S.InvitationCodeFormTitle>
+            <S.InvitationCodeFormInput
+              placeholder="초대코드를 입력해주세요."
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+          </S.InvitationCodeFormInputWrapper>
+          <S.InvitationCodeSubmitButton disabled={!inputValue.trim()}>
+            입장하기
+          </S.InvitationCodeSubmitButton>
+        </S.InvitationCodeForm>
+      </S.InvitationCodeSection>
 
-        <S.RoomListSection>
-          <S.RoomListTitle>방 리스트</S.RoomListTitle>
-          <S.RoomCardWrapper>
-            <RoomCard />
-          </S.RoomCardWrapper>
-        </S.RoomListSection>
-      </S.MainPage>
-    </>
+      <S.RoomListSection>
+        <S.RoomListTitle>방 리스트</S.RoomListTitle>
+        <S.RoomCardWrapper>
+          <RoomCard />
+        </S.RoomCardWrapper>
+      </S.RoomListSection>
+    </S.MainPage>
   );
 };
 
