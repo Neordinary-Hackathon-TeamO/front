0dd704cb6785fa86067cf8c66d9aed4241c1eeb6
DongjaJ <jdh981118@naver.com>
2024-11-23 23:06:17 +0900

    feat: 폰트 적용

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -3,6 +3,7 @@ import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { router } from './router.tsx';
 import 'the-new-css-reset/css/reset.css';
+import './index.css';
 
 const [MILLISECOND, SECOND, MINUTE] = [1000, 60, 5];
 const STALE_TIME = MILLISECOND * SECOND * MINUTE;
