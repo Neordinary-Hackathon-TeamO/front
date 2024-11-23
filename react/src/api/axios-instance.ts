4ea4dd68e001002c486d6876437931461421f356
DongjaJ <jdh981118@naver.com>
2024-11-23 16:07:08 +0900

    feat: axios instance 추가

---
+++ react/src/api/axios-instance.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const REQUEST_TIME = 25000;
+
+export const axiosClient = axios.create({
+  baseURL: import.meta.env.VITE_BASEURL,
+  timeout: REQUEST_TIME,
+});
+
+// TODO 추후 로컬 스토리지 혹은 쿠키와 연동해서 header에 넣는 로직 필요
+const getAuthHeader = () => `bearer accesstoken`;
+
+axiosClient.interceptors.request.use(
+  (config) => {
+    config.headers['Authorization'] = getAuthHeader();
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  },
+);
