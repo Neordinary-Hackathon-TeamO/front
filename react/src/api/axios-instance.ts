f83fbbb4aed510ffb21ec2c7a0df5b8a7a1151a7
DongjaJ <jdh981118@naver.com>
2024-11-23 17:05:37 +0900

    refactor: axiosInstance 이름 수정

--- react/src/api/axios-instance.ts
+++ react/src/api/axios-instance.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const REQUEST_TIME = 25000;
 
-export const axiosClient = axios.create({
+export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BASEURL,
   timeout: REQUEST_TIME,
 });
@@ -10,7 +10,7 @@ export const axiosClient = axios.create({
 // TODO 추후 로컬 스토리지 혹은 쿠키와 연동해서 header에 넣는 로직 필요
 const getAuthHeader = () => `bearer accesstoken`;
 
-axiosClient.interceptors.request.use(
+axiosInstance.interceptors.request.use(
   (config) => {
     config.headers['Authorization'] = getAuthHeader();
     return config;
