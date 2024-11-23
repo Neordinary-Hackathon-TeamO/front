dc5b3934398e8c4d1e4af39bdff3a1e96df6ef66
DongjaJ <jdh981118@naver.com>
2024-11-23 17:04:12 +0900

    chore: router provider, queryClient provider 추가

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { router } from './router.tsx';
+
+const [MILLISECOND, SECOND, MINUTE] = [1000, 60, 5];
+const STALE_TIME = MILLISECOND * SECOND * MINUTE;
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: STALE_TIME,
+      // ✅ turns retries off
+      retry: false,
+    },
+  },
+});
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>,
+);
